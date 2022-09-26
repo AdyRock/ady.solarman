9bde31387a44af75a26a676d0929ad4be4459d91
AdyRock <38184396+AdyRock@users.noreply.github.com>
2022-09-26 11:09:29 +0100

    check for 0 before updating capability

--- drivers/grid/device.js
+++ drivers/grid/device.js
@@ -16,9 +16,7 @@ class GridDevice extends LanDevice
         this.log('GridDevice has been initialized');
     }
 
-    async onSettings({ oldSettings, newSettings, changedKeys })
-    {
-    }
+    async onSettings({ oldSettings, newSettings, changedKeys }) {}
 
     async updateLanDeviceValues(serial, data)
     {
@@ -31,14 +29,34 @@ class GridDevice extends LanDevice
                 this.setAvailable();
 
                 this.setCapabilityValue('measure_power', -data.Grid_Power).catch(this.error);
-                this.setCapabilityValue('measure_voltage', data.Grid_Voltage).catch(this.error);
-                this.setCapabilityValue('measure_current', data.Grid_Current).catch(this.error);
-                this.setCapabilityValue('measure_frequency', data.Grid_Frequency).catch(this.error);
-
-                this.setCapabilityValue('meter_power.today_import', data.Import_Today).catch(this.error);
-                this.setCapabilityValue('meter_power.today_export', data.Export_Today).catch(this.error);
-                this.setCapabilityValue('meter_power.total_import', data.Total_Import).catch(this.error);
-                this.setCapabilityValue('meter_power.total_export', data.Total_Export).catch(this.error);
+                if (data.Grid_Voltage)
+                {
+                    this.setCapabilityValue('measure_voltage', data.Grid_Voltage).catch(this.error);
+                }
+                if (data.Grid_Current)
+                {
+                    this.setCapabilityValue('measure_current', data.Grid_Current).catch(this.error);
+                }
+                if (data.Grid_Frequency)
+                {
+                    this.setCapabilityValue('measure_frequency', data.Grid_Frequency).catch(this.error);
+                }
+                if (data.Import_Today > 0)
+                {
+                    this.setCapabilityValue('meter_power.today_import', data.Import_Today).catch(this.error);
+                }
+                if (data.Export_Today > 0)
+                {
+                    this.setCapabilityValue('meter_power.today_export', data.Export_Today).catch(this.error);
+                }
+                if (data.Total_Import > 0)
+                {
+                    this.setCapabilityValue('meter_power.total_import', data.Total_Import).catch(this.error);
+                }
+                if (data.Total_Export > 0)
+                {
+                    this.setCapabilityValue('meter_power.total_export', data.Total_Export).catch(this.error);
+                }
             }
         }
         catch (err)
--- drivers/solar_panel/device.js
+++ drivers/solar_panel/device.js
@@ -16,9 +16,7 @@ class SolarPanelDevice extends LanDevice
         this.log('SolarPanelDevice has been initialized');
     }
 
-    async onSettings({ oldSettings, newSettings, changedKeys })
-    {
-    }
+    async onSettings({ oldSettings, newSettings, changedKeys }) {}
 
     async updateLanDeviceValues(serial, data)
     {
@@ -30,18 +28,42 @@ class SolarPanelDevice extends LanDevice
             {
                 this.setAvailable();
 
-                this.setCapabilityValue('measure_power', data.PV_Power).catch(this.error);
-                this.setCapabilityValue('meter_power.today', data.Daily_Production).catch(this.error);
-                this.setCapabilityValue('meter_power', data.Total_Generation).catch(this.error);
-
-                this.setCapabilityValue('measure_power.pv1', data.PV1_Power).catch(this.error);
-                this.setCapabilityValue('measure_voltage.pv1', data.PV1_Voltage).catch(this.error);
-                this.setCapabilityValue('measure_current.pv1', data.PV1_Current).catch(this.error);
-
-                this.setCapabilityValue('measure_power.pv2', data.PV2_Power).catch(this.error);
-                this.setCapabilityValue('measure_voltage.pv2', data.PV2_Voltage).catch(this.error);
-                this.setCapabilityValue('measure_current.pv2', data.PV2_Current).catch(this.error);
-
+                if (data.PV_Power > 0)
+                {
+                    this.setCapabilityValue('measure_power', data.PV_Power).catch(this.error);
+                }
+                if (data.Daily_Production > 0)
+                {
+                    this.setCapabilityValue('meter_power.today', data.Daily_Production).catch(this.error);
+                }
+                if (data.Total_Generation > 0)
+                {
+                    this.setCapabilityValue('meter_power', data.Total_Generation).catch(this.error);
+                }
+                if (data.PV1_Power > 0)
+                {
+                    this.setCapabilityValue('measure_power.pv1', data.PV1_Power).catch(this.error);
+                }
+                if (data.PV1_Voltage > 0)
+                {
+                    this.setCapabilityValue('measure_voltage.pv1', data.PV1_Voltage).catch(this.error);
+                }
+                if (data.PV1_Current > 0)
+                {
+                    this.setCapabilityValue('measure_current.pv1', data.PV1_Current).catch(this.error);
+                }
+                if (data.PV2_Power > 0)
+                {
+                    this.setCapabilityValue('measure_power.pv2', data.PV2_Power).catch(this.error);
+                }
+                if (data.PV2_Voltage > 0)
+                {
+                    this.setCapabilityValue('measure_voltage.pv2', data.PV2_Voltage).catch(this.error);
+                }
+                if (data.PV2_Current > 0)
+                {
+                    this.setCapabilityValue('measure_current.pv2', data.PV2_Current).catch(this.error);
+                }
             }
         }
         catch (err)
